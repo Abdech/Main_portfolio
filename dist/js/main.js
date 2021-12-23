03c5990bc23671051f65975ed19fe1fbd9551bfc
Azizsaad <abdech33@gmail.com>
2021-12-24 00:39:52 +0100

    Added some styling, carousel, a calender and a lighbox

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,11 @@ const hamburger = document.querySelector('.menu-btn_burger')
 const nav = document.querySelector('.nav')
 const menuNav = document.querySelector('.menu-nav')
 const menuNavItem = document.querySelectorAll('.menu-nav_item')
+const welcomeText = document.querySelector('.home_name')
+const calender = document.querySelector('.calender')
+const showCalenderBtn = document.querySelector('#show-cal')
+const carouselImages = document.querySelector(".carousel_images")
+const carouselButtons = document.querySelectorAll('carousel_button')
 
 let showMenu = false
 
@@ -17,6 +22,31 @@ menuBtn.addEventListener('click', toggleMenu)
 //     }
 // }
   
+//lightbox
+
+const lightbox = document.createElement('div')
+const img = document.createElement('img')
+const images = document.querySelectorAll('.grid img')
+
+
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        img.src = e.target.src
+
+        lightbox.appendChild(img)
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget ) return
+    lightbox.classList.remove('active')
+})
+// switching between mobile and desktop view
 function toggleMenu() {
     if (!showMenu) {
         hamburger.classList.toggle('open')
@@ -27,3 +57,15 @@ function toggleMenu() {
         })
     }
 } 
+
+
+//introductry animation
+window.addEventListener("load", () => {
+    welcomeText.classList.add('animate');
+})
+
+//show calender 
+showCalenderBtn.addEventListener('click', () => {
+    calender.style.display = 'flex'
+    showCalenderBtn.style.display = "none"
+})
